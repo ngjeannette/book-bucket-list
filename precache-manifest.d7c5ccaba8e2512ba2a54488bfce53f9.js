68b407da0dc9357d87482af6bb03fde23164f961
Jeannette <jeannetteshui.ng@gmail.com>
2020-08-15 14:11:38 -0400

    Updates

---
+++ precache-manifest.d7c5ccaba8e2512ba2a54488bfce53f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df080dac3ffa6d740f15cf6089f071b5",
+    "url": "/book-bucket-list/index.html"
+  },
+  {
+    "revision": "5f0118ac4c04e9c21d81",
+    "url": "/book-bucket-list/static/css/2.11a187e6.chunk.css"
+  },
+  {
+    "revision": "b18910e36ba426793bce",
+    "url": "/book-bucket-list/static/css/main.57cf4931.chunk.css"
+  },
+  {
+    "revision": "5f0118ac4c04e9c21d81",
+    "url": "/book-bucket-list/static/js/2.6950a81a.chunk.js"
+  },
+  {
+    "revision": "7904ecf3b3f323c35964ff86ee306c9a",
+    "url": "/book-bucket-list/static/js/2.6950a81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18910e36ba426793bce",
+    "url": "/book-bucket-list/static/js/main.a7f7b175.chunk.js"
+  },
+  {
+    "revision": "88a5632ee9d65f4b7133",
+    "url": "/book-bucket-list/static/js/runtime-main.711cac59.js"
+  },
+  {
+    "revision": "cbdaca4a40e9c5301e70577445cb561f",
+    "url": "/book-bucket-list/static/media/bookcover.cbdaca4a.jpg"
+  }
+]);
